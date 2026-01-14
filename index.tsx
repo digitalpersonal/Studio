06e399822c806f390a22e1a14feead82789e0925
digitalpersonal <112121450+digitalpersonal@users.noreply.github.com>
2026-01-14 00:58:04 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Fix: Changed from default import to named import for App component
+import { App } from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
